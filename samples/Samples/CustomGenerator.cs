1aa80b7fff4292298ffa6bfa887529aec8cee3af
GitHub Action <hello@clarius.org>
2021-05-23 14:21:05 +0000

    Publish pages from devlooped/avatar@d3a00b25a

---
+++ samples/Samples/CustomGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using Avatars;
+using Xunit;
+
+namespace Samples
+{
+    /// <summary>
+    /// Showcases how an arbitrary method can be annotated as a "avatar generator" that 
+    /// will seamlessly extended default behaviors for new avatars.
+    /// </summary>
+    public class CustomGenerator
+    {
+        [Fact]
+        public void RandomPing()
+        {
+            var ping = Randomizer.Of<IPing>();
+
+            // Each execution results in a new random value.
+            Assert.NotEqual(ping.Ping(), ping.Ping());
+            Assert.NotEqual(ping.Ping(), ping.Ping());
+        }
+    }
+
+    public interface IPing
+    {
+        int Ping();
+    }
+
+    public static class Randomizer
+    {
+        static readonly Random random = new Random();
+
+        [AvatarGenerator]
+        public static T Of<T>()
+            => Avatar.Of<T>().AddBehavior(
+                (invocation, next) => invocation.CreateValueReturn(random.Next()),
+                invocation => invocation.MethodBase is MethodInfo info && info.ReturnType == typeof(int));
+    }
+}
---
+++ samples/Samples/Debugging.cs
@@ -0,0 +1,41 @@
+﻿using Avatars;
+using Xunit;
+
+namespace Samples
+{
+    public class Debugging
+    {
+        /// <summary>
+        /// Set a breakpoint in the last line of the method and inspect the 
+        /// avatar instance to see how Avatar optimizes and improves the 
+        /// troubleshooting and inspection of running avatars and their 
+        /// invocations.
+        /// </summary>
+        [Fact]
+        public void DebuggerDisplay()
+        {
+            var calc = Avatar.Of<ICalculator>();
+            var recorder = new RecordingBehavior();
+
+            calc.AddBehavior(recorder);
+            calc.AddBehavior(new DefaultEqualityBehavior());
+
+            calc.AddBehavior(
+                (invocation, next) => invocation.CreateValueReturn(invocation.Arguments.Get<int>(0) + invocation.Arguments.Get<int>(1)),
+                invocation => invocation.MethodBase.Name == nameof(ICalculator.Add),
+                "Add");
+
+            calc.AddBehavior(new DefaultValueBehavior());
+            calc.GetHashCode();
+
+            Assert.False(calc.Equals(new object()));
+            Assert.Equal(5, calc.Add(3, 2));
+            Assert.Equal(10, calc.Add(5, 5));
+
+            calc.Store("Mem1", 50);
+            calc.Recall("Mem1");
+
+            var calls = recorder.Invocations;
+        }
+    }
+}
